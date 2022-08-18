de898f027575e2552c67cc3e4b80876d7a5965d5
Mohammed Saeed Hansrod <62431166+m-saeed-h@users.noreply.github.com>
2022-08-18 22:44:54 +0200

    update

--- Mohammed-Saeed-Hansrod/app.js
+++ Mohammed-Saeed-Hansrod/app.js
@@ -1,14 +1,3 @@
-// function openNav() {
-//     document.getElementById("static-left-nav-section").style.width = "250px";
-//     document.getElementById("Body").style.marginLeft = "250px";
-//   }
-  
-//   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-//   function closeNav() {
-//     document.getElementById("static-left-nav-section").style.width = "0";
-//     document.getElementById("Body").style.marginLeft = "0";
-//   }
-  
   function openNav() {
     document.getElementById("static-left-nav-section").style.width = "100%";
   
@@ -18,3 +7,20 @@
   function closeNav() {
     document.getElementById("static-left-nav-section").style.width = "0";
   }
+
+
+window.addEventListener('scroll',()=>{
+
+  let content = document.querySelector('footer');
+
+  let contentPosition = content.getBoundingClientRect().top;
+
+  let screenPosition = window.innerHeight;
+
+  if(contentPosition < screenPosition) {
+
+    content.classList.add('footer')
+  }else{
+    content.classList.remove('footer')
+  }
+});
