090624f117deea8f13e58b933cddaefda339b42b
Mohammed Saeed Hansrod <62431166+m-saeed-h@users.noreply.github.com>
2022-08-18 18:01:24 +0200

    updated files

---
+++ Mohammed-Saeed-Hansrod/app.js
@@ -0,0 +1,20 @@
+// function openNav() {
+//     document.getElementById("static-left-nav-section").style.width = "250px";
+//     document.getElementById("Body").style.marginLeft = "250px";
+//   }
+  
+//   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+//   function closeNav() {
+//     document.getElementById("static-left-nav-section").style.width = "0";
+//     document.getElementById("Body").style.marginLeft = "0";
+//   }
+  
+  function openNav() {
+    document.getElementById("static-left-nav-section").style.width = "100%";
+  
+  }
+  
+  /* Close/hide the sidenav */
+  function closeNav() {
+    document.getElementById("static-left-nav-section").style.width = "0";
+  }
